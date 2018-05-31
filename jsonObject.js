337356262ff8897d608defb9cdf852974710577c
Katie elizabeth French <845855192@bun2-206-16.local>
2018-06-01 11:50:57 +1200

    learning JSON objects

---
+++ jsonObject.js
@@ -0,0 +1,45 @@
+// basic suit
+//     100 per day
+//     sizes 40 -46
+// tuxedo
+//     160 per day
+//     sizes 42- 50
+// armani suit
+//     400 per day
+//     sizes 39+46
+
+var arnoldSuits{
+      suits : {
+            basicSuit : {
+                  pricePerDay : 100,
+                  availiableSizes : "40 , 42, 44, 46"
+            },
+            tuxedoSuit : {
+                  pricePerDay : 160,
+                  availiableSizes : "42, 44, 46, 48, 50"
+            },
+            armaniSuit : {
+                  pricePerDay : 400,
+                  availiableSizes : "38, 40, 42, 44, 46"
+            }
+      } //end of "suits"
+} //end of arnoldSuits variable
+
+
+
+var arnoldSuits{
+      "suits" : {
+            "basicSuit" : {
+                  "pricePerDay" : 100,
+                  "availiableSizes" : "40 , 42, 44, 46"
+            },
+            "tuxedoSuit" : {
+                  "pricePerDay" : 160,
+                  "availiableSizes" : "42, 44, 46, 48, 50"
+            },
+            "armaniSuit" : {
+                  "pricePerDay" : 400,
+                  "availiableSizes" : "39, 40, 42, 44, 46"
+            }
+      } //end of "suits"
+} //end of arnoldSuits variable
